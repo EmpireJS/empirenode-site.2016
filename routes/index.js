72154db93073218c2fb021075c74fb46b01efed4
Matt Walters <mattwalters5@gmail.com>
2016-11-07 11:02:00 -0500

    captions redirect

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,9 @@ var routes = {
 exports = module.exports = function (app) {
 	// Views
 	app.get('/', routes.views.index);
+	app.get('/captions', function (req, res) {
+		return res.redirect('http://old.aloft.nu/stanley/empirenode');
+	})
 	app.all('/contact', routes.views.contact);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
